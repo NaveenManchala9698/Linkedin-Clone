04cbf92d21266c58c93b493b34bda0b94896ced6
Naveen Kumar Manchala <100999011+NaveenManchala9698@users.noreply.github.com>
2022-07-11 14:11:01 +0200

    Profile Page

--- bw-linkedin/src/App.js
+++ bw-linkedin/src/App.js
@@ -1,8 +1,13 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Profile from "./components/Profile";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Profile />
+    </div>
+  );
 }
 
 export default App;
