136519b51c1a001d53bbd9bbdc8f1e0712395752
Harika_Kommuri <harikakommuri35@gmail.com>
2022-07-13 16:07:18 +0200

    Added News Feed Profile section

--- bw-linkedin/src/App.js
+++ bw-linkedin/src/App.js
@@ -4,17 +4,18 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import CurrentProfile from "./components/CurrentProfile";
 import Navbar from "./components/Navbar";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import NewsFeedProfile from "./components/NewsFeedProfile";
 
 import Home from "./components/Pages/Home";
 
 function App() {
   return (
     <div className="App">
+      <NewsFeedProfile />
       <BrowserRouter>
-        <Navbar />
+        {/* <Navbar /> */}
         <Routes>
-          <Route path="/" element={<Home />} />
-
+          {/*  <Route path="/" element={<Home />} /> */}
           {/* <CurrentProfile /> */}
         </Routes>
       </BrowserRouter>
