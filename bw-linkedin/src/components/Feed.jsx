f733e93d6fd9648c323494173e96a78e44e61e73
Naveen Kumar Manchala <100999011+NaveenManchala9698@users.noreply.github.com>
2022-07-14 17:57:42 +0200

    Feed updated

--- bw-linkedin/src/components/Feed.jsx
+++ bw-linkedin/src/components/Feed.jsx
@@ -2,17 +2,24 @@ import {
   Arrow90degRight,
   ChatText,
   HandThumbsUp,
+  PencilFill,
   SendFill,
 } from "react-bootstrap-icons";
 import { useEffect, useState } from "react";
 import { Container, Row, Col, Button, Modal, Form } from "react-bootstrap";
 import { format, parseISO } from "date-fns";
 
+import { Link } from "react-router-dom";
+
 const Feed = () => {
   const [feed, setFeed] = useState([]);
 
-  const [show, setShow] = useState(false);
+  const [editPost, setEditPost] = useState({
+    text: "",
+  });
+  const [currentPostID, setCurrentPostID] = useState(null);
 
+  const [show, setShow] = useState(false);
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
 
@@ -20,8 +27,7 @@ const Feed = () => {
     fetchFeed();
   }, []);
 
-  // GET FEED
-
+  //Get
   const fetchFeed = async () => {
     try {
       const url = "https://striveschool-api.herokuapp.com/api/posts/";
@@ -47,8 +53,104 @@ const Feed = () => {
     }
   };
 
+  // PUT POST
+
+  const changePost = async () => {
+    try {
+      let response = await fetch(
+        "https://striveschool-api.herokuapp.com/api/posts/" + currentPostID,
+        {
+          method: "PUT",
+          body: JSON.stringify(editPost),
+          headers: {
+            "Content-Type": "application/json",
+            Authorization:
+              "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MmNiZjY0YmU2YzAzMDAwMTU5MTgxNDUiLCJpYXQiOjE2NTc1MzQwMjcsImV4cCI6MTY1ODc0MzYyN30.CB7NDDp16Z2r4LEBmGrsgrwMVNQI6vKZ1_ERAXJtQyU",
+          },
+        }
+      );
+      if (response.ok) {
+        let resData = await response.json();
+        console.log("Edited Successfully");
+      } else {
+        console.log("ERROR!");
+      }
+    } catch (error) {
+      console.log("Error!");
+    }
+  };
+
+  //Delete Method
+
+  const deletePost = async () => {
+    try {
+      const options = {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization:
+            "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MmNiZjY0YmU2YzAzMDAwMTU5MTgxNDUiLCJpYXQiOjE2NTc1MzQwMjcsImV4cCI6MTY1ODc0MzYyN30.CB7NDDp16Z2r4LEBmGrsgrwMVNQI6vKZ1_ERAXJtQyU",
+        },
+      };
+      const response = await fetch(
+        "https://striveschool-api.herokuapp.com/api/posts/" + currentPostID,
+        options
+      );
+      if (response.ok) {
+        console.log("Deleted Successfully.");
+      } else {
+        console.log("Error!!");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <>
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Edit Post</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form onSubmit={changePost}>
+            <Form.Group>
+              <Form.Label>Edit Text</Form.Label>
+              <Form.Control
+                type="text"
+                value={editPost.text}
+                onChange={(e) =>
+                  setEditPost({ ...editPost, text: e.target.value })
+                }
+              />
+            </Form.Group>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button
+            variant="danger"
+            type="submit"
+            onClick={() => {
+              handleClose();
+              deletePost();
+            }}
+          >
+            Delete
+          </Button>
+          <Button
+            variant="primary"
+            onClick={() => {
+              handleClose();
+              changePost();
+            }}
+          >
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
       {feed &&
         feed
           .reverse()
@@ -56,7 +158,7 @@ const Feed = () => {
           .map((eachFeed) => {
             if (eachFeed.user) {
               return (
-                <div className="feed">
+                <div className="feed" key={eachFeed._id}>
                   <div>
                     <Container>
                       <Row>
@@ -73,19 +175,38 @@ const Feed = () => {
                           />
                         </Col>
                         <Col
-                          xs={10}
+                          xs={9}
                           className="text-left pl-0 mt-1"
                           style={{ lineHeight: "2px" }}
                         >
-                          <h6>
-                            {eachFeed.user.name} {eachFeed.user.surname}
-                          </h6>
+                          <Link
+                            to={`/user/${eachFeed.user._id}`}
+                            style={{ color: "black" }}
+                          >
+                            <h6>
+                              {eachFeed.user.name} {eachFeed.user.surname}
+                            </h6>
+                          </Link>
+
                           <p style={{ fontSize: "10px" }}>
                             {eachFeed.user.title}
                           </p>
                           <p style={{ fontSize: "8px" }}>
                             {format(parseISO(eachFeed.updatedAt), "MMMM dd")}
                           </p>
+                        </Col>
+
+                        <Col xs={1}>
+                          {eachFeed.user._id === "62cbf64be6c0300015918145" ? (
+                            <PencilFill
+                              onClick={() => {
+                                handleShow();
+                                setCurrentPostID(eachFeed._id);
+                              }}
+                            />
+                          ) : (
+                            ""
+                          )}
                         </Col>
                       </Row>
                       <Row>
@@ -129,9 +250,6 @@ const Feed = () => {
                       </Row>
                     </Container>
                   </div>
-                  {/*<h6 style={{ textAlign: "left" }}>{eachFeed.user.name} {eachFeed.user.surname}</h6>
-                          
-                          <p style={{ textAlign: "left" }}>{eachFeed.text}</p>*/}
                 </div>
               );
             }
